23e45d92460efc9b5c2e42fc2349d7651ac315b5
mrdylanroberts <mrdylanroberts@pm.me>
2025-05-29 15:47:28 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from .email_cog import EmailCog
-
-async def setup(bot):
-    cog = EmailCog(bot)
-    await bot.add_cog(cog)
-
-__red_end_user_data_statement__ = "This cog stores email sender configurations for Discord channels and Gmail API credentials locally."
